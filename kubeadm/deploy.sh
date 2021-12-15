b5066517fe50c7c195af4269e15a0a45260d3f9b
Josh Gavant <joshgavant@gmail.com>
2021-12-15 14:43:25 -0600

    update kubeadm section

--- kubeadm/deploy.sh
+++ kubeadm/deploy.sh
@@ -1,56 +1,30 @@
 #! /usr/bin/env -S bash -e
 
 this_dir=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
-root_dir=$(cd "$(dirname ${BASH_SOURCE[0]})/.." && pwd)
+
+export kubernetes_version=${1:-v1.23.0}
+export config_dir=${2:-${this_dir}/config}
 
 temp_config_path=$(mktemp)
-config_dir=${this_dir}/config
-cat ${config_dir}/init.yaml >> ${temp_config_path}
-cat ${config_dir}/cluster.yaml >> ${temp_config_path}
-cat ${config_dir}/kubelet.yaml >> ${temp_config_path}
-cat ${config_dir}/kube-proxy.yaml >> ${temp_config_path}
+cat ${config_dir}/init.yaml | envsubst >> ${temp_config_path}
+echo "" >> ${temp_config_path}
+cat ${config_dir}/cluster.yaml | envsubst >> ${temp_config_path}
 kubeadm init --config ${temp_config_path}
+
 ## for upgrade
-# new_version=1.22.1
-# kubeadm upgrade plan --config ${temp_config_path}
-# kubeadm upgrade apply v${new_version} --config ${temp_config_path}
+# new_version=v1.23.1
+# kubeadm upgrade plan
+# kubeadm upgrade apply ${new_version} --config ${temp_config_path}
 
 ## if calling kubectl manually, copy admin.conf first:
 # mkdir -p $HOME/.kube
 # sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 # sudo chown $(id -u):$(id -g) $HOME/.kube/config
-chmod -R 0644 /etc/kubernetes/*
+chmod -R 0644 /etc/kubernetes/admin.conf
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-# kubectl taint nodes --all node-role.kubernetes.io/master-
+echo "installing calico pod network"
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
-echo "installing tigera-operator"
-kubectl apply -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
-
-# from https://docs.projectcalico.org/manifests/custom-resources.yaml
-echo "installing calico network"
-kubectl apply -f - <<EOF
-# This section includes base Calico installation configuration.
-# For more information, see: https://docs.projectcalico.org/v3.16/reference/installation/api#operator.tigera.io/v1.Installation
-apiVersion: operator.tigera.io/v1
-kind: Installation
-metadata:
-  name: default
-spec:
-  # Configures Calico networking.
-  calicoNetwork:
-    # Note: The ipPools section cannot be modified post-install.
-    ipPools:
-    - blockSize: 26
-      cidr: 10.80.0.0/12
-      encapsulation: VXLANCrossSubnet
-      natOutgoing: Enabled
-      nodeSelector: all()
-    nodeAddressAutoDetectionV4:
-      canReach: '8.8.8.8'
-EOF
-
-# otherwise network calls for DNS (at least) fail...
-firewall-cmd --add-masquerade --permanent
-firewall-cmd --reload
-# kubectl set env -n calico-system daemonset/calico-node IP_AUTODETECTION_METHOD=can-reach=8.8.8.8
+# firewall-cmd --add-masquerade --permanent
+# firewall-cmd --reload
--- kubeadm/upgrade.sh
+++ kubeadm/upgrade.sh
@@ -1,18 +1,5 @@
 #! /usr/bin/env -S bash -e
 
-this_dir=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
-root_dir=$(cd "$(dirname ${BASH_SOURCE[0]})/.." && pwd)
-
-temp_config_path=$(mktemp)
-config_dir=${this_dir}/config
-cat ${config_dir}/init.yaml >> ${temp_config_path}
-cat ${config_dir}/cluster.yaml >> ${temp_config_path}
-cat ${config_dir}/kubelet.yaml >> ${temp_config_path}
-cat ${config_dir}/kube-proxy.yaml >> ${temp_config_path}
-
-new_version=1.22.3
+export kubernetes_version=${1:-v1.23.0}
 # kubeadm upgrade plan "v${new_version}"
-kubeadm upgrade apply v${new_version}
-
-# echo "installing tigera-operator"
-# kubectl apply -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+kubeadm upgrade apply ${kubernetes_version}
