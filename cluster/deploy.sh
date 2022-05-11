cb8a193df80f3e989de364bc53a7af1be468e360
Josh Gavant <joshgavant@gmail.com>
2022-05-11 13:22:33 -0500

    bump cluster version

--- cluster/deploy.sh
+++ cluster/deploy.sh
@@ -2,29 +2,33 @@
 
 this_dir=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
 
-export kubernetes_version=${1:-v1.23.0}
+export kubernetes_version=${1:-v1.24.0}
 export config_dir=${2:-${this_dir}/config}
 
+echo "INFO: kubeadm version"
+kubeadm version
+echo "INFO: kubernetes version to install: ${kubernetes_version}"
+
+echo "INFO: rendering and concatenating config files"
 temp_config_path=$(mktemp)
 cat ${config_dir}/init.yaml | envsubst >> ${temp_config_path}
 echo "" >> ${temp_config_path}
 cat ${config_dir}/cluster.yaml | envsubst >> ${temp_config_path}
-kubeadm init --config ${temp_config_path}
 
-## for upgrade
-# new_version=v1.23.1
-# kubeadm upgrade plan
-# kubeadm upgrade apply ${new_version} --config ${temp_config_path}
+echo ""
+echo "INFO: running kubeadm init"
+kubeadm init --config ${temp_config_path}
 
 ## if calling kubectl manually, copy admin.conf first:
 # mkdir -p $HOME/.kube
 # sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 # sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 chmod -R 0644 /etc/kubernetes/admin.conf
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 echo "installing calico pod network"
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
-# firewall-cmd --add-masquerade --permanent
-# firewall-cmd --reload
+echo "registering local persistent volume and storageclass"
+kubectl apply -f ${this_dir}/pv.yaml
